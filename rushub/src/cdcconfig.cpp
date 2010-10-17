9fa8418d6425289aa407efbdfb5facd6dc896077
setuper <setuper@gmail.com>
2010-10-17 14:07:08 +0000

    fix dir for lang

--- rushub/src/cdcconfig.cpp
+++ rushub/src/cdcconfig.cpp
@@ -261,13 +261,13 @@ void cDCLang::AddVars()
 bool cDCLang::Load()
 {
   string sFile(mDCServer->mMainPath.msConfPath + string("lang.xml"));
-  return mDCServer->mConfigLoader.Load(this, sFile);
+  return mDCServer->mConfigLoader.Load(this, sFile.c_str());
 }
 
 bool cDCLang::Save()
 {
   string sFile(mDCServer->mMainPath.msConfPath + string("lang.xml"));
-  return mDCServer->mConfigLoader.Save(this, sFile);
+  return mDCServer->mConfigLoader.Save(this, sFile.c_str());
 }
 
 }; // nDCServer
