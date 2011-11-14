0bdb80b65f0a7a6003b9b07ab1e099d7ff8643dd
setuper <setuper@gmail.com>
2011-11-14 18:06:01 +0000

    set TCP_NODELAY

--- rushub/src/Conn.cpp
+++ rushub/src/Conn.cpp
@@ -282,10 +282,18 @@ tSocket Conn::socketCreate(const char * port, const char * address, bool udp) {
 	}
 
 	if (!udp) {
-		sockoptval_t yes = 1;
+		sockoptval_t so_reuseaddr = 1;
+		sockoptval_t tcp_nodelay = 1;
 
 		// TIME_WAIT after close conn. Reuse address after disconn
-		if (SOCK_ERROR(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(sockoptval_t)))) {
+		if (SOCK_ERROR(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof(sockoptval_t)))) {
+			if (log(FATAL)) {
+				logStream() << "Error in setsockopt: " << SOCK_ERR_MSG << " [" << SOCK_ERR << "]" << endl;
+			}
+			return INVALID_SOCKET;
+		}
+
+		if (SOCK_ERROR(setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &tcp_nodelay, sizeof(sockoptval_t)))) {
 			if (log(FATAL)) {
 				logStream() << "Error in setsockopt: " << SOCK_ERR_MSG << " [" << SOCK_ERR << "]" << endl;
 			}
