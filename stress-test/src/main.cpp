b6972fb03b2a48799f979c42926c6d2f3f5d3eeb
setuper <setuper@gmail.com>
2012-02-20 19:17:01 +0000

    fix stress-test

--- stress-test/src/main.cpp
+++ stress-test/src/main.cpp
@@ -1,4 +1,23 @@
+/*
+ * RusHub - hub server for Direct Connect peer to peer network.
+ *
+ * Copyright (C) 2009-2012 by Setuper
+ * E-Mail: setuper at gmail dot com (setuper@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include "stdinc.h"
 #include "Server.h"
 #include "Conn.h"
 
@@ -163,7 +182,7 @@ private:
 	}
 
 	int onTimer(Time & now) {
-		__int64 diff = now.msec() - mChecker.msec();
+		int64_t diff = now.msec() - mChecker.msec();
 		if (diff >= 0 ? diff >= mTimerServPeriod : -diff >= mTimerServPeriod) {
 			mChecker = now;
 			if (mConnCount < mMaxConn) {
